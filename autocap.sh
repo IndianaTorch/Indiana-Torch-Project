005fa8739cb6aeea9ac958721cab1cd3cfd7edfa
Beaglebone <jdurec@purdue.edu>
2015-04-01 05:51:36 +0000

    torch

--- autocap.sh
+++ autocap.sh
@@ -3,3 +3,10 @@ cd ~/
 cd ..
 cd project
 fswebcam -r 1920x1080 -d /dev/video0 /project/autocap.jpg
+FILE=autocap.jpg
+NAME=${FILE%.*}
+EXT=${FILE#*.}
+DATE=`date +%y-%m-%d@%H:%M:%S`
+NEWFILE=${NAME}_${DATE}.${EXT}
+echo $NEWFILE
+mv /project/autocap.jpg /project/$NEWFILE
--- button.py
+++ button.py
@@ -1,5 +1,7 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
+import os
+
 GPIO.setup("P8_19", GPIO.IN)
 GPIO.setup("P8_13", GPIO.OUT)
 state = 0
@@ -8,7 +10,9 @@ while True:
     state = GPIO.input("P8_19")
     if state == 1:
         GPIO.output("P8_13", GPIO.HIGH)
-        #print "button pressed!"
+	os.system('./autocap.sh')        
+	#print "button pressed!"
+	print "Ready!"
     if state == 0:
         GPIO.output("P8_13", GPIO.LOW)
     time.sleep(0.1)
