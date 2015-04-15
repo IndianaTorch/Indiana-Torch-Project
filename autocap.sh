cce9ed48413a44df0b529ce1a13d52f9ebc583b2
beagle2 <jdurec@purdue.edu>
2015-04-15 13:20:20 -0400

    beagle2

--- autocap.sh
+++ autocap.sh
@@ -2,11 +2,11 @@
 cd ~/
 cd ..
 cd project
-fswebcam -r 1920x1080 -d /dev/video0 /project/autocap.jpg
+fswebcam -r 1920x1080 -d /dev/video0 /project/pictures/autocap.jpg
 FILE=autocap.jpg
 NAME=${FILE%.*}
 EXT=${FILE#*.}
 DATE=`date +%y-%m-%d@%H:%M:%S`
 NEWFILE=${NAME}_${DATE}.${EXT}
 echo $NEWFILE
-mv /project/autocap.jpg /project/$NEWFILE
+mv /project/pictures/autocap.jpg /project/pictures/$NEWFILE
