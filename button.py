88c5f301ba27beae0cfa5f5226704705c34495df
beagle2 <jdurec@purdue.edu>
2015-04-17 01:51:53 -0400

    beagle2

--- button.py
+++ button.py
@@ -2,17 +2,17 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 import os
 
-GPIO.setup("P8_19", GPIO.IN)
-GPIO.setup("P8_13", GPIO.OUT)
+GPIO.setup("P9_12", GPIO.IN)
+GPIO.setup("P9_11", GPIO.OUT)
 state = 0
 output = 0
 while True:
-    state = GPIO.input("P8_19")
+    state = GPIO.input("P9_12")
     if state == 1:
-        GPIO.output("P8_13", GPIO.HIGH)
-	os.system('./autocap.sh')        
+        GPIO.output("P9_11", GPIO.HIGH)
+	#os.system('./autocap.sh')        
 	#print "button pressed!"
-	print "Ready!"
+	#print "Ready!"
     if state == 0:
-        GPIO.output("P8_13", GPIO.LOW)
+        GPIO.output("P9_11", GPIO.LOW)
     time.sleep(0.1)
