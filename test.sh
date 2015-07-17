982f3dfa11ddf18fdc66748350ccec0dae9cf5a7
Jeffrey Chan <jdurec@purdue.edu>
2015-07-17 00:56:40 +0000

    beagle1

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 python GPS_test.py &
-python cam_button.py &
+python timecao.py &
 python timeup.py &
+#wget -O /dev/null http://download.fedoraproject.org/pub/fedora/linux/releases/21/Workstation/x86_64/iso/Fedora-Live-Workstation-x86_64-21-5.iso &
+
+
+
